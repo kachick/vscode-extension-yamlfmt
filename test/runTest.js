920b544703422134d55ec75c4fce19fa66ed553f
Kenichi Kamiya <kachick1@gmail.com>
2024-05-13 18:28:57 +0900

    Put own yamlfmt config (#23)

--- test/runTest.js
+++ test/runTest.js
@@ -1,28 +1,39 @@
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
 
 const { runTests } = require("@vscode/test-electron");
 
+const projectConfigPath = "yamlfmt.yml";
+const backupConfigPath = `${projectConfigPath}.bak`;
+
+// The folder containing the Extension Manifest package.json
+// Passed to `--extensionDevelopmentPath`
+const extensionDevelopmentPath = path.resolve(__dirname, "../");
+
+// The path to the extension test script
+// Passed to --extensionTestsPath
+const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+
 async function main() {
+  let exitcode = 1;
+
+  fs.renameSync(projectConfigPath, backupConfigPath);
+
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, "../");
-
-    // The path to the extension test script
-    // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-
     // Download VS Code, unzip it and run the integration test
-    await runTests({
+    exitcode = await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
       launchArgs: ["--user-data-dir", path.join(os.tmpdir(), "yamlfmt-test")],
     });
   } catch (err) {
     console.error("Failed to run tests", err);
-    process.exit(1);
+  } finally {
+    fs.renameSync(backupConfigPath, projectConfigPath);
   }
+
+  process.exit(exitcode);
 }
 
 main();
