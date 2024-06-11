1f8a4c893dd3edd534263dacb406f36d511ff8c9
Kenichi Kamiya <kachick1@gmail.com>
2024-06-11 21:00:58 +0900

    Bump and pin to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   inputs = {
     # How to update the revision
     #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,17 +10,15 @@
     {
       self,
       nixpkgs,
-      edge-nixpkgs,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
       in
       {
-        formatter = edge-pkgs.nixfmt-rfc-style;
+        formatter = pkgs.nixfmt-rfc-style;
         devShells.default =
           with pkgs;
           mkShell {
@@ -30,13 +27,13 @@
               # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
               bashInteractive
               nil
-              edge-pkgs.nixfmt-rfc-style
+              nixfmt-rfc-style
 
-              edge-pkgs.nodejs_20
-              edge-pkgs.yamlfmt
-              edge-pkgs.deno
-              edge-pkgs.dprint
-              edge-pkgs.typos
+              nodejs_20
+              yamlfmt
+              deno
+              dprint
+              typos
             ];
           };
       }
